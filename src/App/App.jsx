5a2c8307b17b594383d7c8071e7b2a6507b3b3ef
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2023-09-24 19:28:30 +0300

    add start blog

---
+++ src/App/App.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header/index.jsx";
+
+function App() {
+
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+function Header() {
+	return (
+		<header>
+			<div className="header">
+				<ul className="nav">
+					<li><a href="#!">Home</a></li>
+					<li><a href="#!">Blog</a></li>
+					<li><a href="#!">About</a></li>
+				</ul>
+			</div>
+		</header>
+	);
+}
+export default Header;
