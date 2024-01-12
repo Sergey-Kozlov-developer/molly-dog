76174702ee6d71393d64930a95ace0edd51de9a2
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2024-01-12 15:39:45 +0300

    add mokky api

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,38 +2,38 @@ import styles from "./Card.module.scss";
 import { motion } from "framer-motion";
 
 function Card({ imageUrl, title, subTitle, description, date }) {
-  return (
-    <div className={styles.blog}>
-      <div className={styles.cards_item}>
-        <motion.div
-          className={styles.card}
-          initial={{ scale: 0 }}
-          animate={{ scale: 1 }}
-          transition={{ delay: 0.5 }}
-        >
-          <div className={styles.card_image}>
-            <span className={styles.note}>&copy;Molly</span>
-            <motion.img
-              src={imageUrl}
-              alt="Dog"
-              // initial={{ scale: 0 }}
-              // animate={{ scale: 1 }}
-              // transition={{ delay: 0.5 }}
-            />
-            <span className={styles.card_price}>{date}</span>
-          </div>
-          <div className={styles.card_content}>
-            <h1 className={styles.card_title}>{title}</h1>
-            <div className={styles.card_text}>
-              <h2>{subTitle}</h2>
-              <hr />
-              <p>{description}</p>
+    return (
+        <div className={styles.blog}>
+            <div className={styles.cards_item}>
+                <motion.div
+                    className={styles.card}
+                    initial={{ scale: 0 }}
+                    animate={{ scale: 1 }}
+                    transition={{ delay: 0.5 }}
+                >
+                    <div className={styles.card_image}>
+                        <span className={styles.note}>&copy;Molly</span>
+                        <motion.img
+                            src={imageUrl}
+                            alt="Dog"
+                            // initial={{ scale: 0 }}
+                            // animate={{ scale: 1 }}
+                            // transition={{ delay: 0.5 }}
+                        />
+                        <span className={styles.card_price}>{date}</span>
+                    </div>
+                    <div className={styles.card_content}>
+                        <h1 className={styles.card_title}>{title}</h1>
+                        <div className={styles.card_text}>
+                            <h2>{subTitle}</h2>
+                            <hr />
+                            <p>{description}</p>
+                        </div>
+                    </div>
+                </motion.div>
             </div>
-          </div>
-        </motion.div>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default Card;
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -8,46 +8,49 @@ import { setCategoryId } from "../redux/slices/filterSlice.js";
 import axios from "axios";
 
 export const Blog = () => {
-  // useState получение поста
-  const [items, setItems] = useState([]);
-  // useState подгрузка placeholder
-  const [isLoading, setIsLoading] = useState(true);
-  // hook redux вытаскиваем определенное из state фильтра
-  const categoryId = useSelector((state) => state.filter.categoryId);
-  // dispatch(redux) передает действие(например передает id выбора категории)
-  const dispatch = useDispatch();
-  // функция выбора категории
-  const onChangeCategory = (id) => {
-    dispatch(setCategoryId(id));
-  };
+    // useState получение поста
+    const [items, setItems] = useState([]);
+    // useState подгрузка placeholder
+    const [isLoading, setIsLoading] = useState(true);
+    // hook redux вытаскиваем определенное из state фильтра
+    const categoryId = useSelector((state) => state.filter.categoryId);
+    // dispatch(redux) передает действие(например передает id выбора категории)
+    const dispatch = useDispatch();
+    // функция выбора категории
+    const onChangeCategory = (id) => {
+        dispatch(setCategoryId(id));
+    };
 
-  useEffect(() => {
-    setIsLoading(true);
-    const category = categoryId > 0 ? `category=${categoryId}` : "";
-    axios
-      .get(`https://65226adaf43b179384147b19.mockapi.io/items?${category}`)
-      .then(
-        (res) => {
-          setItems(res.data);
-          setIsLoading(false);
-        },
-        [categoryId]
-      );
-  }, [categoryId]);
+    useEffect(() => {
+        setIsLoading(true);
+        const category = categoryId > 0 ? `category=${categoryId}` : "";
+        axios.get(`https://439c4dd6b6a6a8f1.mokky.dev/items?${category}`).then(
+            (res) => {
+                setItems(res.data);
+                setIsLoading(false);
+            },
+            [categoryId]
+        );
+    }, [categoryId]);
 
-  return (
-    <>
-      <div className="container">
-        <Categories value={categoryId} onChangeCategory={onChangeCategory} />
-        {/*грузим  плайсхолдер*/}
-        {isLoading ? (
-          <Skeleton />
-        ) : (
-          items.map((element) => <Card key={element.id} {...element} />)
-        )}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="container">
+                <Categories
+                    value={categoryId}
+                    onChangeCategory={onChangeCategory}
+                />
+                {/*грузим  плайсхолдер*/}
+                {isLoading ? (
+                    <Skeleton />
+                ) : (
+                    items.map((element) => (
+                        <Card key={element.id} {...element} />
+                    ))
+                )}
+            </div>
+        </>
+    );
 };
 
 export default Blog;
