9fb23f3d6427d54238c7ea71f4ebbaca9bbafaa3
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2024-01-13 19:54:35 +0300

    add icon

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qctid15uv6"
+    "revision": "0.6p2p5a4slsg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,32 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      devOptions: {
-        enabled: true,
-      },
-    }),
-  ],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            outDir: "build",
+            manifest: {
+                name: "Molly the dog",
+                short_name: "Molly",
+                description: "Сайт про жизнь нашей собаки",
+                theme_color: "#ffffff",
+                icons: [
+                    {
+                        src: "images/android-icon-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "images/ms-310x310.png",
+                        sizes: "310x310",
+                        type: "image/png",
+                    },
+                ],
+            },
+            devOptions: {
+                enabled: true,
+            },
+        }),
+    ],
 });
